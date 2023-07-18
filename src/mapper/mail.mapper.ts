ce220d4aa074dc48c0dbe08bd559f7783dec3719
Tom <tomc@tomvisions.com>
2023-07-18 19:22:47 -0400

    adding debug code

--- src/mapper/mail.mapper.ts
+++ src/mapper/mail.mapper.ts
@@ -95,6 +95,8 @@ export class MailMapper {
 
 
     async apiSendMail() {
+        console.log(this._params);
+        console.log(await this._sesClient.send(new SendTemplatedEmailCommand(this._params)));
         return await this._sesClient.send(new SendTemplatedEmailCommand(this._params));
     }
 
