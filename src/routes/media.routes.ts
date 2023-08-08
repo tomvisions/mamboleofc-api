7b95f7f5963a65e6134657dcda0f479b3d3cb08a
Tom <tomc@tomvisions.com>
2023-08-08 11:31:31 -0400

    added more routes

--- src/routes/media.routes.ts
+++ src/routes/media.routes.ts
@@ -6,7 +6,9 @@ import {GalleryController} from "../controllers/gallery.controller";
 import {GalleryImageController} from "../controllers/gallery.image.controller";
 
 mediaRouter.post("/", GalleryController.apiCreateGallery);
-mediaRouter.get("/", GalleryController.apiGetAllGalleries);
+mediaRouter.get("/slug/:slug", GalleryController.apiGetAllGalleries);
+mediaRouter.get("/primary", GalleryController.apiGetAllGalleries);
+
 mediaRouter.post("/image", GalleryImageController.apiUploadImage);
 mediaRouter.post("/images", GalleryImageController.apiUploadImages);
 
